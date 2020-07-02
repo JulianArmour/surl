8aadfc3f086c21f2c242d9c39eae3e11f7929bdd
julian <julian.armour2@mail.mcgill.ca>
2020-07-02 17:25:58 -0400

    base_39_encode tested

--- smurl/shortener.py
+++ smurl/shortener.py
@@ -2,4 +2,11 @@ alphabet = "bcdfghjkmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ"
 
 
 def base_39_encode(number):
-    return alphabet[number]
+    if number == 0:
+        return 'b'
+
+    encoding = []
+    while number > 0:
+        encoding.append(alphabet[number % 39])
+        number //= 39
+    return "".join(reversed(encoding))
--- tests/test_shortener.py
+++ tests/test_shortener.py
@@ -7,11 +7,27 @@ alphabet = "bcdfghjkmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ"
 
 @pytest.mark.parametrize(
     "test_input,expected",
-    [(0, alphabet[0]), (1, alphabet[1]), (len(alphabet) - 1, alphabet[-1])],
+    [
+        # single characters
+        (0, alphabet[0]),
+        (1, alphabet[1]),
+        (38, "Z"),
+        # two characters
+        (39, "cb"),
+        (40, "cc"),
+        (39 * 2, "db"),
+        (39 * 2 + 1, "dc"),
+        (39 * 2 + 38, "dZ"),
+        (39 * 3, "fb"),
+        (39 * 4, "gb"),
+        # three characters
+        (39 ** 2, "cbb"),
+        (39 ** 2 + 1, "cbc"),
+        (2 * 39 ** 2, "dbb"),
+        # four characters
+        (39 ** 3, "cbbb"),
+    ],
 )
-def test_encode_single_digit(test_input, expected):
+def test_base_39_encode(test_input, expected):
     assert shortener.base_39_encode(test_input) == expected
 
-
-def test_encode_double_digit():
-    assert shortener.base_39_encode(39) == "bb"
