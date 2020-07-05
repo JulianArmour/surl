bfaa593e51cef3e72e67258e3a601283753f2d7d
julian <julian.armour2@mail.mcgill.ca>
2020-07-05 18:16:19 -0400

    test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,17 +9,23 @@ from smurl.db import get_db
 
 @pytest.fixture
 def app():
-    # db_fd, db_path = tempfile.mkstemp()
+    db_fd, db_path = tempfile.mkstemp()
     app = create_app(
-        test_config={"TESTING": True, "DATABASE": ":memory:", "SECRET_KEY": "TEST"}
+        test_config={"TESTING": True, "DATABASE": db_path, "SECRET_KEY": "TEST"}
     )
-
     with app.app_context():
         db = get_db()
         with app.open_resource("schema.sql") as schema:
             db.executescript(schema.read().decode("UTF-8"))
+        with open(os.path.join(os.path.dirname(__file__), "test_data.sql"), "rb") as f:
+            db.executescript(f.read().decode("UTF-8"))
 
     yield app
 
-    # os.close(db_fd)
-    # os.unlink(db_path)
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
---
+++ tests/test_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO UrlMap (original_url, short_hash)
+VALUES ('www.example.com/something', 'short')
