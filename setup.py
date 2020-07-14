9dedd5b5b336f624d7c0e2b577eeb1fde7d34e61
julian <julian.armour2@mail.mcgill.ca>
2020-07-14 09:17:26 -0400

    made project installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="surl",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["flask==1.1.2", "flask-restful==0.3.8"],
+)
