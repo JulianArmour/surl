2b4d0cc3ea26ab3e61846214362922ef12eebdff
julian <julian.armour2@mail.mcgill.ca>
2020-07-14 23:04:12 -0400

    add CLI command to create database

--- surl/db.py
+++ surl/db.py
@@ -1,6 +1,9 @@
+import os
 import sqlite3
 
+import click
 from flask import current_app, g
+from flask.cli import with_appcontext
 
 
 def get_db():
@@ -18,5 +21,20 @@ def close_db(e=None):
         db.close()
 
 
+@click.command("create-db", )
+@with_appcontext
+def create_db():
+    if os.path.exists(current_app.config["DATABASE"]):
+        raise click.ClickException(
+            "A database already exists at the location specified in the DATABASE configuration"
+        )
+
+    db = get_db()
+    with current_app.open_resource("schema.sql") as schema:
+        db.executescript(schema.read().decode("UTF-8"))
+    click.echo("Created database!")
+
+
 def init_app(app):
     app.teardown_appcontext(close_db)
+    app.cli.add_command(create_db)
