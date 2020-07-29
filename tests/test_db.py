4f3ed3683f70e46dd6f26b03722023a3b5fb20d2
julian <julian.armour2@mail.mcgill.ca>
2020-07-29 19:52:32 -0400

    tested database creation cli command

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,10 @@
+import os
 import sqlite3
+import tempfile
 
 import pytest
 
+from surl import create_app
 from surl.db import get_db, close_db
 
 
@@ -27,3 +30,26 @@ def test_close_db(app):
         close_db()
         with pytest.raises(sqlite3.ProgrammingError):
             db.execute("SELECT 1")
+
+
+def test_create_db_already_exist():
+    db_fd, db_path = tempfile.mkstemp()
+    app = create_app(test_config={"TESTING": True, "DATABASE": db_path})
+    cli = app.test_cli_runner()
+
+    result = cli.invoke(args=["create-db"])
+    assert "A database already exists" in result.output
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+def test_create_db_uncreated():
+    db_path = os.path.join(tempfile.gettempdir(), "a_temp_db.sqlite")
+    app = create_app(test_config={"TESTING": True, "DATABASE": db_path})
+    cli = app.test_cli_runner()
+
+    result = cli.invoke(args=["create-db"])
+    assert "Created database" in result.output
+
+    os.unlink(db_path)
