2d121bbcfd259b9b5b469f5acbc8adaabd0f040f
julian <julian.armour2@mail.mcgill.ca>
2020-08-06 15:17:16 -0400

    change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="surl",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     zip_safe=False,
